00c3242671f399a34f856e6913d35422f5a8a77a
Kaka8548 <katyakatya8548@gmail.com>
2022-11-24 13:14:11 +0400

    Додано прослуховувач подій по сабміту форми; створено шаблон фотокарток для галереї

--- src/PixabayApi.js
+++ src/PixabayApi.js
@@ -5,16 +5,18 @@ export class PixabayApi {
   #API_URL = `https://pixabay.com/api/`;
 
   constructor() {
-    this.query = 'dog';
+    this.query = null;
   }
 
-  fetchPhotos(query) {
+  fetchPhotos() {
     const searchParams = new URLSearchParams({
       key: this.#API_KEY,
       q: this.query,
       image_type: 'photo',
       orientation: 'horizontal',
       safesearch: true,
+      page: 1,
+      per_page: 10,
     });
 
     return axios.get(`${this.#API_URL}?${searchParams}`);
--- src/index.js
+++ src/index.js
@@ -1,6 +1,36 @@
 import './scss/index.scss';
 import { PixabayApi } from './PixabayApi';
+import galleryPhotoTemplate from './galleryPhotoTemplate';
+import Notiflix from 'notiflix';
+
+const searchFormEl = document.querySelector('.search-form');
+const galleryEl = document.querySelector('.gallery');
+
+function showResult(response) {
+  console.log(response);
+  const { data } = response;
+  const photos = galleryPhotoTemplate(data);
+  galleryEl.innerHTML = photos;
+
+  if (data.hits.length === 0) {
+    Notiflix.Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+  }
+}
 
 const pixabayApi = new PixabayApi();
 
-pixabayApi.fetchPhotos().then(response => console.log(response));
+function onSearchFormElSubmit(event) {
+  event.preventDefault();
+  pixabayApi.query = event.target[0].value;
+
+  pixabayApi
+    .fetchPhotos()
+    .then(response => {
+      showResult(response);
+    })
+    .catch(err => console.log(err));
+}
+
+searchFormEl.addEventListener('submit', onSearchFormElSubmit);
